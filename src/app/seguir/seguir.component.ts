73ec08042ac0156548ad9b6ad3701a1495b11719
Pedro Hernández <pedro-cun-1405@hotmail.com>
2019-11-14 21:52:06 -0700

    Proceso completo, solo falta pantalla a donde dirigir una vez terminado el proceso

--- src/app/seguir/seguir.component.ts
+++ src/app/seguir/seguir.component.ts
@@ -14,6 +14,8 @@ export class SeguirComponent implements OnInit {
   dato: String;
   usuario: String;
 
+  public encontro : boolean;
+
   public user : String;
 
   constructor( private activatedRoute: ActivatedRoute,private dataApi: DataApiService) {
@@ -28,14 +30,29 @@ export class SeguirComponent implements OnInit {
     this.creadores = [["Creador 1","franciscleylim"],["Creador 2","ProAndroid"],["Creador 3","AnnaCLXY"],["Creador 4","Corona_MX"],["Creador 5","CarameloYoutube"]];
     this.dato= this.creadores[this.valor][1];
     this.user = "";
+    this.encontro = false;
 
   }
 
   getDataApi(){
+    
     if(this.user !="" && this.user !=" "){
       this.dataApi.usuario = this.user;
-      this.dataApi.getDatos().subscribe(data => console.log(data));
-    }else{alert("Favor de ingresar usuario de twitter");}
+      this.dataApi.getDatos().subscribe( (data : any) => {
+        if(data["data"]){
+          for(var i = 0; i < data["data"]["users"].length; i++){
+            if(data["data"]["users"][i]["screen_name"] == this.dato){ this.encontro = true; break;}
+          }
+          if(this.encontro){
+            this.encontro = false;
+            alert("Gracias por completar el proceso");
+          }else{
+            alert("Favor de seguir al creador para poder continuar");
+            console.log(data);
+          }
+        }else{alert("Nombre de usuario invalido");}
+      });
+    }else{alert("Favor de llenar campo 'username'");}
   }
 
 }
